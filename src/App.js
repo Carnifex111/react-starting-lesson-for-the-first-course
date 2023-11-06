33e123b1973cd385aaa3b7c26f92cf0d2104488e
Shirov Ivan <117617823+Carnifex111@users.noreply.github.com>
2023-11-06 18:08:31 +0500

    useState2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
   function handleClick() {
     setCount(count + 1);
+  }
+
+  function handleTextChange(event) {
+    setText(event.target.value);
   }
 
   return (
     <div>
       <p>Ты нажал на кнопку {count} раз!</p>
       <button onClick={handleClick}>Нажми на меня</button>
+
+      <br />
+
+      <input type="text" value={text} onChange={handleTextChange} />
+      <p>Здесь отображается все, что ты напечатал: {text}</p>
     </div>
   );
 }
